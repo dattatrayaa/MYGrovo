3859df952e5a765844ebc2e2ee8fb699a2da38a8
Dattatraya Aidale <37131980+dattatrayaa@users.noreply.github.com>
2018-07-20 15:08:06 +0530

    Update BaseTestClass.py

--- TestCases/BaseTestClass.py
+++ TestCases/BaseTestClass.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver=webdriver.Chrome("chromedriver")
+driver=webdriver.Chrome()
 
 class BaseTestClass:
     
