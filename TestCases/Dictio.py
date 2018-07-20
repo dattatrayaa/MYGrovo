3daadd3db62ccd2241c6880e517fbac1efd2aa99
Dattatraya Aidale <37131980+dattatrayaa@users.noreply.github.com>
2018-07-20 14:38:31 +0530

    Add files via upload

---
+++ TestCases/Dictio.py
@@ -0,0 +1,31 @@
+'''
+Created on 13-Apr-2018
+
+@author: dattatraya
+'''
+
+
+class Diction:
+    
+    def d(self):
+        myD={'camp':'"//div/button[1]"'}
+        print myD['camp']
+        
+        
+        
+        d={'name':'is', 'raja':'rani'}
+        
+        print d.keys()
+        print d.values()
+        
+        print 10/2
+        
+        
+        
+        
+        
+        
+        
+if __name__=='__main__':
+    c=Diction()
+    c.d()
